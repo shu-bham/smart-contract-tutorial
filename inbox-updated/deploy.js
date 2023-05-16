30c180a25831a99b17402948cb8c5826879c6573
shubhamcs <shubham.bagrecha@go-mmt.com>
2023-05-16 07:59:27 +0530

    add new project with upgraded sol ver

---
+++ inbox-updated/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "walk company crash mandate easily lemon wine sock apart ship antenna salt",
+  "https://sepolia.infura.io/v3/ea7a9f960a774bc28995ee16f435901e"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
